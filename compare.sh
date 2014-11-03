7ef9563bff8250fde885422f97badfd910600af4
Josh Smith <joshsmith2@gmail.com>
2014-11-03 17:30:39 +0000

    Size and ls checks working - vimdiff functionality a TODO until it's needed.

--- compare.sh
+++ compare.sh
@@ -5,8 +5,11 @@
 
 roots_file='./roots.txt'
 path=''
+list_dir=false
+tree_diff=false
+full_diff=false
 
-while getopts "r:p:h" opt; do
+while getopts "r:p:hltf" opt; do
     case $opt in 
         r)
             roots_file=$OPTARG
@@ -14,21 +17,57 @@ while getopts "r:p:h" opt; do
         p)
             path=$OPTARG
             ;;
+        l)
+            list_dir=true
+            ;;
+        t)
+            tree_diff=true
+            ;;
+        f)
+            full_diff=true
+            ;;
         h)
             echo "This script will allow you to compare folder structures located at distinct roots."
             echo "Usage:"
-            echo "    -r : roots_file : The root locations in which to look for the file"
-            echo "    -p : path : Path to the file or folder you'd like information on."
+            echo "    -r roots_file : The root locations in which to look for the file"
+            echo "    -p path : Path to the file or folder you'd like information on."
+            echo "    -l : Print a directory listing of each path"
+            echo "    -t : Open a vimdiff of each path's tree - WIP"
+            echo "    -f : Open a vimdiff of every file within each path - WIP"
     esac
 done
 
-echo "Roots file (should contain locations to search within): $roots_file" >&2
-if [[ $path=='' ]]; then
-    echo "Please specify a path using the '-p' flag, or run this with '-h' for help."
+if [[ -z $path ]]; then
+    echo "Error: Please specify a path using the '-p' flag, or run this with '-h' for help."
     exit
 else
     echo "Path to search for: $path"
 fi
+echo "Roots file (should contain locations to search within): $roots_file" >&2
 
+roots_string=''
 
-            
+echo "Checking paths:"
+for root in `cat $roots_file`; do
+    roots_string="$roots_string $root"
+    full_path=$root$path
+    echo $full_path
+    total_size=`du -hc $full_path | tail -n 1`
+    echo "Size: $total_size"
+    if $list_dir; then
+        echo "ls -laGh:"
+        ls -laGh $full_path
+    fi
+    echo ""
+done
+
+# WIP: Will be developed further if needed.
+#if $tree_diff; then
+#    echo "Press enter to compare trees of roots using vimdiff, or n to cancel"
+#    read tree
+#    if [ $tree!='n' ]; then
+#        for r in $roots_string; do
+#        vimdiff $tree_paths
+#    fi
+#fi
+
